eb43e9b0383755dfe6a31e77c10dd4f4e716b6e3
Michael Ballou <mikeblaa@yahoo.com>
2025-12-05 17:10:22 -0600

    day-5: part 1 in progress

---
+++ 2025/day-5/cafeteria.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""
+cafeteria.py - Advent of Code 2025 Day 5
+
+Run:
+    python cafeteria.py --input input.txt
+"""
+
+from __future__ import annotations
+import argparse
+import sys
+from pathlib import Path
+from typing import List, Tuple, Any
+
+
+def parse_args(argv: List[str] | None = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Advent of Code 2025 - Day 2: Gift Shop")
+    parser.add_argument("-i", "--input", type=Path, default=None, help="Path to input file (defaults to stdin)")
+    return parser.parse_args(argv)
+
+
+def read_input(path: Path | None) -> str:
+    if path is None:
+        return sys.stdin.read()
+    return path.read_text()
+
+
+def parse_input(raw: str) -> Any:
+    """
+    Parse raw input text into a structured form.
+    Replace the body of this function with logic specific to the puzzle input.
+    """
+    # lines = [line.strip() for line in raw.splitlines() if line.strip()]
+    lines = [line for line in raw.splitlines()]
+    return lines
+
+
+def solve_part1(data: Any) -> Any:
+    """Solve part 1. Return result (int, str, etc.)."""
+    
+    # Find the first empty line, lines before are ranges of fresh ingredients, 
+    # lines after are ingredients to check
+    x = data.index("")
+
+    fresh_ranges = data[:x]
+    check_ingredients = data[x+1:]
+    fresh_set = set()
+    for r in fresh_ranges:
+        start, end = range_to_start_end(r)
+        add_range_to_set(fresh_set, start, end)
+
+    count_fresh = 0
+    for ingredient in check_ingredients:
+        if int(ingredient) in fresh_set:
+            count_fresh += 1
+
+    return count_fresh
+
+
+def solve_part2(data: Any) -> Any:
+    """Solve part 2. Return result (int, str, etc.)."""
+    # TODO: implement
+    return None
+
+"""Part 1 methods"""
+def add_range_to_set(s: set[int], start: int, end: int) -> None:
+    for i in range(start, end + 1):
+        s.add(i)
+
+def range_to_start_end(r: str) -> Tuple[int, int]:
+    start_str, end_str = r.split("-")
+    return int(start_str), int(end_str)
+
+def main(argv: List[str] | None = None) -> int:
+    args = parse_args(argv)
+    raw = read_input(args.input)
+    data = parse_input(raw)
+
+    part1 = solve_part1(data)
+    part2 = solve_part2(data)
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
---
+++ 2025/day-5/test_cafeteria.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import unittest
+from cafeteria import *
+
+class CafeteriaTest(unittest.TestCase):
+    
+    def test_add_range_to_set(self):
+        s = set()
+        add_range_to_set(s, 5, 10)
+        self.assertTrue(s == {5, 6, 7, 8, 9, 10})
+        self.assertTrue(5 in s)
+        self.assertFalse(4 in s)
+
+        add_range_to_set(s, 15,20)
+        self.assertTrue(s == {5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20})
+
+    def test_range_to_start_end(self):
+        r = "5-10"
+        start, end = range_to_start_end(r)
+        self.assertEqual(start, 5)
+        self.assertEqual(end, 10)
+
+        r = "0-0"
+        start, end = range_to_start_end(r)
+        self.assertEqual(start, 0)
+        self.assertEqual(end, 0)
+        
+if __name__ == "__main__":
+    unittest.main()
